dd7de0c6c1ad50a8a9b57227e5d4de9715c9761b
Peer Hoins <peer.overbeck@gmail.com>
2023-01-06 09:26:34 +0100

    First attempt of releasing my own aws provider frankenstein

--- main.go
+++ main.go
@@ -30,7 +30,7 @@ func main() {
 	log.SetFlags(logFlags)
 
 	err = tf5server.Serve(
-		"registry.terraform.io/hashicorp/aws",
+		"registry.terraform.io/peov/aws",
 		serverFactory,
 		serveOpts...,
 	)
